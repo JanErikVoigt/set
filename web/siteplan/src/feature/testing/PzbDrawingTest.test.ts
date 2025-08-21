8d52b56aa411a0c71d8d701ab76735297bc6c282
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-08-21 11:37:19 +0200

    setup jest; add prototype test for PZB drawing

---
+++ web/siteplan/src/feature/testing/PzbDrawingTest.test.ts
@@ -0,0 +1,9 @@
+import PZBFeature from "../PZBFeature";
+
+describe('greet function', () => {
+  it('describe behaviour', () => {
+    //let model = []
+    //let feature = PZBFeature.getFeatures()
+    expect('Hello, John!').toEqual('Hello, John!');
+  });
+});
