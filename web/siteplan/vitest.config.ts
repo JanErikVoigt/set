969c0c5555138f45561cc47c8b59547adab4b28c
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-08-22 13:10:48 +0200

    configure vitest

---
+++ web/siteplan/vitest.config.ts
@@ -0,0 +1,15 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'rbush': __dirname + '/node_modules/rbush/rbush.js'
+    },
+    globals: true,
+    environment: 'jsdom',
+    include: ['**/*.test.{js,ts}']
+    // ...
+  }
+})
