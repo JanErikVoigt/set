6fb1f438789908fdae321da049b55eb16e3a10d0
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-08-22 13:11:28 +0200

    add pseudo tests to check setup

--- web/siteplan/src/service/SvgServiceTests/pzb.test.js
+++ web/siteplan/src/service/SvgServiceTests/pzb.test.js
@@ -1,8 +1,21 @@
-import { expect, test } from 'vitest'
-import { axios, AxiosStatic } from 'axios'
-import SvgService from '@/service/SvgService'
+
+
+import SvgService from '@/service/SvgService';
+import { axios } from 'axios';
+import { expect, test } from 'vitest';
+
+test('vitest is set up properly', () => {
+  expect(typeof window).not.toBe('undefined')
+})
+
+// An entry will be shown in the report for this suite
+describe.todo('unimplemented suite')
+
 
 test('adds 1 + 2 to equal 3', () => {
+
+    
     const svg_service = new SvgService(axios)
   expect(1+2).toBe(3)
 })
+
