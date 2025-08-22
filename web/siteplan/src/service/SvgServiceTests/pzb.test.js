e3bf3901c1d2e6acda73ff333b420f643963033c
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-08-22 13:28:08 +0200

    attempt also importing vue in test config

--- web/siteplan/src/service/SvgServiceTests/pzb.test.js
+++ web/siteplan/src/service/SvgServiceTests/pzb.test.js
@@ -1,8 +1,36 @@
 
-
+global.ResizeObserver = class ResizeObserver {
+    observe() {
+      // do nothing
+    }
+    unobserve() {
+      // do nothing
+    }
+    disconnect() {
+      // do nothing
+    }
+  };
 import SvgService from '@/service/SvgService';
 import { axios } from 'axios';
-import { expect, test } from 'vitest';
+import { beforeAll, expect, test } from 'vitest';
+
+beforeAll(() => {
+  global.ResizeObserver = class ResizeObserver {
+    observe() {
+      // do nothing
+    }
+    unobserve() {
+      // do nothing
+    }
+    disconnect() {
+      // do nothing
+    }
+  };
+});
+
+
+
+
 
 test('vitest is set up properly', () => {
   expect(typeof window).not.toBe('undefined')
@@ -15,7 +43,7 @@ describe.todo('unimplemented suite')
 test('adds 1 + 2 to equal 3', () => {
 
     
-    const svg_service = new SvgService(axios)
+    const svgService = new SvgService(axios)
   expect(1+2).toBe(3)
 })
 
--- web/siteplan/vitest.config.ts
+++ web/siteplan/vitest.config.ts
@@ -1,7 +1,9 @@
+import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
     alias: {
       '@': path.resolve(__dirname, './src'),
