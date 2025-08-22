5bd05f9219b73c094cc1d82b0c3fa416f83ee77d
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-08-22 14:26:27 +0200

    use vitest-browser mode (experimental) for testing

--- web/siteplan/vitest.config.ts
+++ web/siteplan/vitest.config.ts
@@ -1,15 +1,23 @@
-import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    browser: {
+      provider: 'playwright', // or 'webdriverio'
+      enabled: true,
+      // at least one instance is required
+      instances: [{ browser: 'chromium' }]
+    }
+  }
+
+/*  test: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       'rbush': __dirname + '/node_modules/rbush/rbush.js'
     },
     globals: true,
     environment: 'edge-runtime',
-    include: ['**/*.test.{js,ts}']
+    include: ['***.test.{js,ts}']
     // ...
-  }
+  } */
 })
