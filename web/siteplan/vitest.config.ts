b03f99829c18ab6dc361d4e3bdf7cc2e99ece046
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-08-22 13:31:09 +0200

    dont import vue in vitest config

--- web/siteplan/vitest.config.ts
+++ web/siteplan/vitest.config.ts
@@ -1,9 +1,7 @@
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [vue()],
   test: {
     alias: {
       '@': path.resolve(__dirname, './src'),
