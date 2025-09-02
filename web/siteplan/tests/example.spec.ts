45fe77a6fc5c54f2c3feda241e05d1ab44fa37a3
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-09-02 15:49:40 +0200

    work on sample spec

--- web/siteplan/tests/example.spec.ts
+++ web/siteplan/tests/example.spec.ts
@@ -1,18 +1,28 @@
-import { test, expect } from '@playwright/test';
+import SvgService from '@/service/SvgService'
+import { expect, test } from '@playwright/test'
+import axios from 'axios'
+
+test('setup SvgService', async ({ page }) => {
+  console.log('hello!')
+  // const axios: AxiosStatic
+  const svgService = new SvgService(axios)
+
+  // const style = svgService.getFeatureStyle(pzb, FeatureType.PZB, label)
+})
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle(/Playwright/)
+})
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/')
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click()
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()
+})
